d16a7d6016165dacb78386f62289fedeb25ac9c3
zicolino <167160363+zicolino@users.noreply.github.com>
2024-12-09 19:43:25 -0300

    terminando sections y corrigiendo styling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//NAV ANIMATIONS
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -11,6 +12,7 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
+//home buttons
 const cvBtn = document.querySelector("#cv-btn");
 cvBtn.addEventListener('click', () => {
   let cvUrl = './assets/cv.pdf';
@@ -28,4 +30,32 @@ const linkedinBtn = document.querySelector("#linkedin-btn");
 linkedinBtn.addEventListener('click', () => {
   let linkedinUrl = 'https://www.linkedin.com/in/nicolasquiroga1/';
   let newTab = window.open(linkedinUrl);
+});
+
+//PROJECT BUTTONS
+
+//PROJECT 1
+const project1git = document.getElementById("project1-git-btn");
+project1git.addEventListener('click', () => {
+  let a = 'https://github.com/nicolasquirogadev/JR-landingpage';
+  let newTab = window.open(a);
+});
+
+const project1live = document.getElementById("project1-live-btn");
+project1live.addEventListener('click', () => {
+  let a = 'https://nicolasquirogadev.github.io/JR-landingpage/';
+  let newTab = window.open(a);
+});
+
+//PROJECT 2
+const project2git = document.getElementById("project2-git-btn");
+project2git.addEventListener('click', () => {
+  let a = 'https://github.com/nicolasquirogadev/blackJack21';
+  let newTab = window.open(a);
+});
+
+const project2live = document.getElementById("project2-live-btn");
+project2live.addEventListener('click', () => {
+  let a = 'https://nicolasquirogadev.github.io/blackJack21/';
+  let newTab = window.open(a);
 });
